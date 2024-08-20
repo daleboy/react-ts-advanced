6b07308289f4c7d7fba17523b088b3ff23969a95
daleboy <785909868@qq.com>
2024-08-21 07:37:49 +0800

    Add Input and Button component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import './App.css'
-import Input from './components/Input'
+import "./App.css";
+import Input from "./components/Input";
+import Button from "./components/Button";
 
 function App() {
-
   return (
     <main>
-      <Input id="name" label="name"/>
-      <Input id="age" label="age"/>
+      <Input id="name" label="name" type="text" />
+      <Input id="age" label="age" type="number" />
+      <p>
+        <Button el="button">Button</Button>
+      </p>
+      <p>
+        <Button el="anchor" href="http://www.google.com">Link</Button>
+      </p>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,15 @@
+import { type ComponentPropsWithoutRef } from "react";
+
 type InputProps = {
-    id:string;
-    label:string;
-}
-function Input({id,label}:InputProps){
+    readonly id:string;
+    readonly label:string;
+}&ComponentPropsWithoutRef<'input'>;
+
+function Input({id,label,...props}:InputProps){
     return(
         <main>
             <label htmlFor={id}>{label}</label>
-            <input id={id} name ={id}/>
+            <input id={id} name ={id} {...props}/>
         </main>
     )
 }
