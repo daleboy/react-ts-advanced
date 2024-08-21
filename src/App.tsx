12028c25922eb1276c800b3a3d98650f7282bc57
daleboy <785909868@qq.com>
2024-08-21 22:15:47 +0800

    design the ReactTimer UI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,18 @@
 import "./App.css";
-import Input from "./components/Input";
-import Button from "./components/Button";
-import Container from "./components/Container";
-import { useRef } from "react";
-import {type FormHandler} from "./components/Form";
-import Form from "./components/Form";
-
+import Header from "./components/Header";
+import AddTimer from "./components/AddTimer";
+import Timers from "./components/Timers"
 function App() {
-  const name = useRef<HTMLInputElement>(null);
-  const age = useRef<HTMLInputElement>(null);
-  const form = useRef<FormHandler>(null);
-  function save(data: unknown) {
-    const formData = data as { name: string; age: number };
-    console.log(formData);
-    if (name.current) {
-      name.current.value = "";
-    }
-    form.current?.clear()
-  }
   return (
-    <Form ref={form} onSave={save}>
-      <Input id="name" label="name" type="text" ref={name} />
-      <Input id="age" label="age" type="number" ref={age} />
-      {/* <p>
-        <Button>submit</Button>
-      </p> */}
-      <p>
-        <Button href="http://www.google.com">A Link</Button>
-      </p>
-      <p>
-        <Container as={Button} type="submit">
-          Click Me
-        </Container>
-      </p>
-    </Form>
+    <>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </>
   );
 }
+
 
 export default App;
---
+++ src/components/AddTimer.tsx
@@ -0,0 +1,25 @@
+import { useRef } from "react";
+import Form from "./UI/Form";
+import Input from "./UI/Input";
+import Button from "./UI/Button";
+import { type FormHandle } from "./UI/Form";
+
+export default function AddTimer() {
+    const form = useRef<FormHandle>(null);
+  
+    function handleSaveTimer(data: unknown) {
+      const extractedData = data as { name: string; duration: string };
+      console.log(extractedData);
+      form.current?.clear();
+    }
+  
+    return (
+      <Form ref={form} onSave={handleSaveTimer} id="add-timer">
+        <Input type="text" label="Name" id="name" />
+        <Input type="number" label="Duration" id="duration" />
+        <p>
+          <Button>Add Timer</Button>
+        </p>
+      </Form>
+    );
+  }
